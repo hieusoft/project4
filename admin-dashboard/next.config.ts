7f3f37529b6817916031dad55bd74d6b4ffa9f02
ducdm1508 <duc.dm.2442@aptechlearning.edu.vn>
2026-07-24 13:31:43 +0700

    fix3

--- admin-dashboard/next.config.ts
+++ admin-dashboard/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://kong:8000/api/:path*", // Proxy to Kong API Gateway
+      },
+    ];
+  },
 };
 
 export default nextConfig;
